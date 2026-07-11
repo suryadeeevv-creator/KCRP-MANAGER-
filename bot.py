f720d0ae9d3e791a41fc3ef08774298334367faf
suryadeeevv-creator <suryadeeevv@gmail.com>
2026-07-12 01:50:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,16 +14,12 @@ intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
+async def setup_hook():
+    await bot.load_extension("cogs.tickets")
+    await bot.tree.sync()
+
+@bot.event
 async def on_ready():
     print(f"✅ {bot.user} is online!")
-    try:
-        synced = await bot.tree.sync()
-        print(f"✅ Synced {len(synced)} slash commands.")
-    except Exception as e:
-        print(e)
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send("🏓 Pong!")
 
 bot.run(TOKEN)

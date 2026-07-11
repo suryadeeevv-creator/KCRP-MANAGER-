c8510a3cc6b3ea36930280a396b973f37fb37cfb
suryadeeevv-creator <suryadeeevv@gmail.com>
2026-07-12 01:37:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,29 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
 
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ {bot.user} is online!")
+    try:
+        synced = await bot.tree.sync()
+        print(f"✅ Synced {len(synced)} slash commands.")
+    except Exception as e:
+        print(e)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("🏓 Pong!")
+
+bot.run(TOKEN)
